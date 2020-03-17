db5f519e2cb18ea96a5ce60fb85ad102348dcbf7
Brandon Marrero <brandonmar97@gmail.com>
2020-03-17 00:49:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @current_name = dog_name
+  end 
+  
+  def name
+    @current_name  
+  end 
+  
+  def bark
+    puts "woof!"
+  end 
+end
