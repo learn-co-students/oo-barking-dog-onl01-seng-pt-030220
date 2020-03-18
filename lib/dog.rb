4e8b16f466b83dd5467f5c2fcd0925d51f25bfc3
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-18 00:20:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)
+    @the_new_doggo = dog_name
+  end
+
+  def name
+    @the_new_doggo
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
