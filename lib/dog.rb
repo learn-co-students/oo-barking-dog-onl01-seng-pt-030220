2cd47ceb0521a6c3062f701292d921eae99f3dfb
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-29 23:20:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+class Dog 
+  
+  
+  def name=(dogs_name)
+    
+    #instance variable to hold dogs name to be used 
+    @name_of_dog = dogs_name
+  end
+  
+  
+  def name
+    #instance variable to get name 
+    @name_of_dog
+  end
+  
+  
+  def bark
+    
+    puts "woof!" 
+    
+  end
+  
+ 
+  
+end#end class
