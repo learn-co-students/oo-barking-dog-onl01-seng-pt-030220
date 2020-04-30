853d106fffdbf38a4176f3f8df875218be2c74d2
celestesmthomas <celestesmthomas@gmail.com>
2020-04-29 21:09:59 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,16 @@
 class Dog
-  
-  def name=(dogs_name)
-    @this_is_the_dogs_name = dogs_name
-  end
-  
-  def name
-    @this_is_the_dogs_name
+  def name=(name)
+    @dog_name = name
   end
 
-  
+  def name
+    @dog_name
+  end
+
   def bark
     puts "woof!"
   end
 end
-
 
 
 
